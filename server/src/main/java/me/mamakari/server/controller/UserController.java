5964ea5e933f6ce2f4f1764baed76440e940ac18
mamakari8842 <nkhj201917@gmail.com>
2026-05-11 00:15:05 +0900

    feat: prepare CORS settings for Vite and initial API client - Add @CrossOrigin to PostContoller and UserController for http://localhost:5173

--- server/src/main/java/me/mamakari/server/controller/UserController.java
+++ server/src/main/java/me/mamakari/server/controller/UserController.java
@@ -5,12 +5,14 @@ import me.mamakari.server.model.User;
 import me.mamakari.server.repository.PostRepository;
 import me.mamakari.server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
